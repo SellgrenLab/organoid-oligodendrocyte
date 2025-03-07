ed6a62ee2c62ebf38fc5c3cd75bb12b3add2c494
Susmita Malwade <56251389+sm2695@users.noreply.github.com>
2025-03-07 08:24:22 +0100

    Update crosstalk.R

--- code/crosstalk.R
+++ code/crosstalk.R
@@ -20,6 +20,57 @@ cellchat@meta$labels <- as.factor(cellchat@meta$labels)
 CellChatDB <- CellChatDB.human # use Ce
 cellchat@DB <- CellChatDB
 
+cellchat <- subsetData(cellchat) # This step is necessary even if using the whole database
+future::plan("multisession", workers = 4) # do parallel
+cellchat <- identifyOverExpressedGenes(cellchat)
+cellchat <- identifyOverExpressedInteractions(cellchat)
+
+cellchat <-  computeCommunProb(cellchat, type = "triMean") # takes forever and is not parallelized
+
+cellchat <- filterCommunication(cellchat, min.cells = 10)
+
+cellchat <- computeCommunProbPathway(cellchat)
+
+cellchat <- aggregateNet(cellchat)
+execution.time = Sys.time() - ptm
+print(as.numeric(execution.time, units = "secs"))
+groupSize <- as.numeric(table(cellchat@idents))
+
+pathways.show <- c("GAS")
+
+par(mfrow=c(1,1))
+netVisual_aggregate(cellchat, signaling = pathways.show, layout = "chord")
+
+levels(cellchat1@idents)
+
+
+
+names(group.cellType) <- levels(cellchat@idents)
+
+netVisual_chord_cell(cellchat, signaling = pathways.show, group = group.cellType, title.name = paste0(pathways.show, " signaling network"))
+
+netVisual_chord_gene(cellchat, sources.use = 4, targets.use = c(5:11), lab.cex = 0.5,legend.pos.y = 30)
+
+
+LR.show <- pairLR.CXCL[1,] # show one ligand-receptor pair
+# Hierarchy plot
+vertex.receiver =  c(8,11)# a numeric vector
+netVisual_individual(cellchat, signaling = pathways.show,  pairLR.use = LR.show)[[1]]
+
+
+netVisual_individual(cellchat1, signaling = pathways.show, pairLR.use = LR.show, layout = "chord", group = group.cellType)
+
+group.cellType <- levels(cellchat1@idents)
+
+group.cellType[c(2,3,9)] <- "Neurons"
+group.cellType[c(4,6,8,13,14,4)] <- "OPCs"
+
+names(group.cellType) <- levels(cellchat1@idents)
+
+netVisual_bubble(cellchat, remove.isolate = FALSE, signaling="GAS")
+
+
+
 
 #### merge everything
 
@@ -117,6 +168,6 @@ netVisual_bubble(cellchat2,signaling = pathways.show[1] ,remove.isolate = FALSE)
 saveRDS(cellchat2,"/Volumes/projects/C3_Sellgren_lab/Lab Members/Susmita/Internal data/Oligo/scRNAseq/processed_data/Oligo_cellchat2.rds")
 
 
-pdf("/Users/susmita.malwade/cellchat2_chord_plot_ALL.pdf")
-netVisual_chord_gene(cellchat2, sources.use = c(1,2,6), targets.use = c(3,4,5,8,9), slot.name = "netP", legend.pos.x = 15, legend.pos.y = 30,   lab.cex = 0.5)
+pdf("/Users/susmita.malwade/cellchat2_chord_plot_ALL2.pdf")
+netVisual_chord_gene(cellchat2, sources.use = c(1,2,6,7), targets.use = c(3,4,5,8,9), slot.name = "netP", legend.pos.x = 15, legend.pos.y = 30,   lab.cex = 0.5)
 dev.off()
